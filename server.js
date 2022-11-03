75a8bd385b5ab72ebbafe83ed34cd2e366a0330f
amohajerani6 <116121374+amohajerani6@users.noreply.github.com>
2022-11-03 15:10:18 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const https = require('https');
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/todoList")
+
+mongoModel = mongoose.model('toDo', {
+  'item': String
+})
 
 const app = express()
 app.set('view engine', 'ejs');
@@ -16,24 +23,36 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 var day = today.getDay();
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday","Saturday"]
-var day=days[day]
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var day = days[day]
 
-today = day+', '+mm + '/' + dd + '/' + yyyy;
-var toDoItem=''
-var items=[]
+today = day + ', ' + mm + '/' + dd + '/' + yyyy;
+var toDoItem = ''
 
 app.get('/', function(req, res) {
-  res.render('index', {today:today, toDoItems:items})
+  mongoModel.find({},
+    function(err, results) {
+      if (results) {
+        var items = results
+      } else {
+        var items = []
+      }
+      res.render('index', {
+        today: today,
+        toDoItems: items
+      })
+    })
 })
 
 // till here
 
-app.post('/', function(req, res)
-{
-  items.push(req.body.toDoInput)
+app.post('/', function(req, res) {
+  newItem = new mongoModel({
+    item: req.body.toDoInput
+  })
+  newItem.save()
   res.redirect('/')
 
 })
 
-app.listen(process.env.PORT || 3000, console.log('listening'))
+app.listen(3000, console.log('listening'))
